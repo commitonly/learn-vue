f8c262c17807ef1d22e153b153dd47bb749c644b
taemin <myloopmy@gmail.com>
2023-02-05 21:21:01 +0900

    vue 페이지 개발 시작전

---
+++ vue-cli/src/components/a.vue
@@ -0,0 +1,20 @@
+<template>
+  <!-- HTML -->
+  <div>header</div>
+</template>
+
+
+
+<script>
+export default {
+// JavsScript - 인스턴스 옵션
+  methods: {
+    addNum: function () {
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+// Css
+</style>
